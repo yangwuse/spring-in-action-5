05441cd99a0eb93744ed41bf5d3d61d1c4500b26
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-09-03 16:35:01 +0800

    m

---
+++ ch01-taco/src/test/java/com/yw/ch01taco/Ch01TacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yw.ch01taco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch01TacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
